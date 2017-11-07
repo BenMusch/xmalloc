5406f94c8bb856dad13248d7ae6dbc5e6a253ecb
Ben Muschol <benmuschol@gmail.com>
2017-11-07 22:32:03 +0000

    First implementation: Only have size 4096

--- par_malloc.c
+++ par_malloc.c
@@ -7,24 +7,17 @@
 #include "bens/hw07/hmalloc.h"
 #include "xmalloc.h"
 
-/*
-  typedef struct hm_stats {
-  long pages_mapped;
-  long pages_unmapped;
-  long chunks_allocated;
-  long chunks_freed;
-  long free_length;
-  } hm_stats;
-*/
-
 typedef struct mem_node {
 	size_t size;
 	struct mem_node* next;
 } mem_node;
 
 const size_t PAGE_SIZE = 4096;
-static hm_stats stats; // This initializes the stats to 0.
-static mem_node* free_list;
+
+const size_t NUM_BINS = 1;
+const size_t BIN_SIZES[] = {4096};
+static mem_node* bins[1];
+
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static
@@ -53,11 +46,35 @@ set_size(void* item, size_t size)
 	*alloc_size = size;
 }
 
+// Returns the bin number to store a elements of the passed size
+int
+get_bin_number(size_t size)
+{
+    for (int i = 0; i < NUM_BINS - 1; i++) {
+        if (size <= BIN_SIZES[i]) {
+            return i;
+        }
+    }
+
+    return NUM_BINS - 1;
+}
+
+size_t
+get_rounded_size(size_t size)
+{
+    for (int i = 0; i < NUM_BINS; i++) {
+        if (size <= BIN_SIZES[i]) {
+            return BIN_SIZES[i];
+        }
+    }
+
+    return -1;
+}
+
 // Initializes pages new memory of as a mem_node
 mem_node*
 mem_node_init(size_t pages)
 {
-	stats.pages_mapped += pages;
 	size_t size = pages * PAGE_SIZE;
 	mem_node* n = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
 	n->size = size;
@@ -65,82 +82,58 @@ mem_node_init(size_t pages)
 	return n;
 }
 
-// "pops" a mem_node with size bytes available off of the free_list
+// "pops" the first element off of the passed bin
 mem_node*
-free_list_pop(size_t size)
+bin_pop(int bin)
 {
-	pthread_mutex_lock(&mutex);
-	mem_node* prev = NULL;
-	mem_node* cur = free_list;
-
-	while (cur != NULL) {
-		if (cur->size > size) {
-			if (prev != NULL) {
-				prev->next = cur->next;
-			} else {
-				// if prev is null, cur == free_list
-				free_list = cur->next;
-			}
-
-			cur->next = NULL;
-			pthread_mutex_unlock(&mutex);
-			return cur;
-		}
-
-		prev = cur;
-		cur = cur->next;
+    pthread_mutex_lock(&mutex);
+	mem_node* node = bins[bin];
+	if (node != NULL) {
+		bins[bin] = node->next;
 	}
-	pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex);
+	return node;
+}
+
+// "pops" a mem_node from the smallest bin that can fulfill size
+mem_node*
+bins_pop(size_t size)
+{
+	size_t rounded_size = get_rounded_size(size);
+    for (int i = 0; i < NUM_BINS; i++) {
+        if (BIN_SIZES[i] >= rounded_size && bins[i] != NULL) {
+            return bins_pop(i);
+        }
+    }
 	return NULL;
 }
 
+// Inserts into the passed bin, if the size is correct
+void
+bin_insert(mem_node* node, int bin_number)
+{
+    pthread_mutex_lock(&mutex);
+    if (bin_number >= NUM_BINS || node->size != BIN_SIZES[bin_number]) {
+        printf("ERROR: Invalid insert of %lu into bin %lu\n", node->size, BIN_SIZES[bin_number]);
+    } else {
+        node->next = bins[bin_number];
+        bins[bin_number] = node;
+    }
+    pthread_mutex_unlock(&mutex);
+}
+
+
 // Inserts into the free list, maintaining sorted order
 void
-free_list_insert(mem_node* node)
+bins_insert(mem_node* node)
 {
-	pthread_mutex_lock(&mutex);
-	node->next = free_list;
-	free_list = node;
-	pthread_mutex_unlock(&mutex);
-}
-
-long
-free_list_length()
-{
-	mem_node* cur = free_list;
-	long len = 0;
-
-	while (cur != NULL) {
-		len++;
-		cur = cur->next;
-	}
-
-	return len;
-}
-
-hm_stats*
-hgetstats()
-{
-    stats.free_length = free_list_length();
-    return &stats;
-}
-
-void
-hprintstats()
-{
-    stats.free_length = free_list_length();
-    fprintf(stderr, "\n== husky malloc stats ==\n");
-    fprintf(stderr, "Mapped:   %ld\n", stats.pages_mapped);
-    fprintf(stderr, "Unmapped: %ld\n", stats.pages_unmapped);
-    fprintf(stderr, "Allocs:   %ld\n", stats.chunks_allocated);
-    fprintf(stderr, "Frees:    %ld\n", stats.chunks_freed);
-    fprintf(stderr, "Freelen:  %ld\n", stats.free_length);
+	int bin_number = get_bin_number(node->size);
+	bin_insert(node, bin_number);
 }
 
 void*
 xmalloc(size_t size)
 {
-    stats.chunks_allocated += 1;
     size += sizeof(size_t);
 
 	mem_node* to_alloc = NULL;
@@ -148,23 +141,16 @@ xmalloc(size_t size)
 	size_t pages = div_up(size, PAGE_SIZE);
 
 	if (pages == 1) {
-		to_alloc = free_list_pop(size);
+		size = get_rounded_size(size);
+		to_alloc = bins_pop(size);
+
+		if (to_alloc == NULL) {
+			to_alloc = mem_node_init(1);
+			// TODO: With smaller bins, distribute this node
+		}
 	} else {
 		size = pages * PAGE_SIZE;	
-	}
-
-	if (to_alloc == NULL) {
 		to_alloc = mem_node_init(pages);
-	}
-
-	size_t new_node_size = to_alloc->size - size;	
-
-	// I know this looks redundant, but for some reason the > 0
-	// conditional prevents a segfault on one of my test machines
-	if (new_node_size > 0 && new_node_size > sizeof(mem_node)) {
-		new_node = ((void*) to_alloc) + size;
-		new_node->size = new_node_size;
-		free_list_insert(new_node);
 	}
 
 	void* item = ((void*) to_alloc) + sizeof(size_t);
@@ -176,18 +162,15 @@ xmalloc(size_t size)
 void
 xfree(void* item)
 {
-    stats.chunks_freed += 1;
-
 	size_t size = get_size(item);
 	item = item - sizeof(size_t);
 
 	if (size >= PAGE_SIZE) {
 		munmap(item, size);
-		stats.pages_unmapped += size / PAGE_SIZE;
 	} else {
 		mem_node* new_node = (mem_node*) (item);
 		new_node->size = size;
-		free_list_insert(new_node);
+		bins_insert(new_node);
 	}
 }
 
